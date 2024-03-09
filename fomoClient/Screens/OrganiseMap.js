6f283aae075c1913511cf2e0274cda64546cfc8a
Ayeshamjad <ayeshaamjad016@gmail.com>
2024-03-10 02:19:04 +0500

    2nd milestone 70%

--- fomoClient/Screens/OrganiseMap.js
+++ fomoClient/Screens/OrganiseMap.js
@@ -1,84 +1,114 @@
-import React, { useRef } from 'react';
-import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { Ionicons } from '@expo/vector-icons';
 
 const OrganiseMap = (navigation) => {
     const mapRef = useRef(null);
+    const [searchQuery, setSearchQuery] = useState('');
 
     const handleContinuePress = () => {
-    //   navigation.navigate('Preference');
+        // navigation.navigate('Preference');
     };
+
     const handleShowLocation = () => {
         if (mapRef.current) {
-          navigator.geolocation.getCurrentPosition(
-            position => {
-              const { latitude, longitude } = position.coords;
-              mapRef.current.animateToRegion({
-                latitude,
-                longitude,
-                latitudeDelta: 0.05,
-                longitudeDelta: 0.05,
-              });
-            },
-            error => console.error(error),
-            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-          );
+            navigator.geolocation.getCurrentPosition(
+                position => {
+                    const { latitude, longitude } = position.coords;
+                    mapRef.current.animateToRegion({
+                        latitude,
+                        longitude,
+                        latitudeDelta: 0.05,
+                        longitudeDelta: 0.05,
+                    });
+                },
+                error => console.error(error),
+                { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+            );
         }
-      };
-    
-      return (
-        <View style={styles.container}>
-          <View style={styles.searchBar}>
-            
-            <Text>Search Bar</Text>
-          </View>
-          <View style={styles.mapContainer}>
-            <MapView
-              style={styles.map}
-              initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-              }}
-              ref={mapRef}
-            >
-              <Marker
-                coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
-                title="Marker"
-                description="This is a marker"
-              />
-            </MapView>
-            <TouchableOpacity style={styles.locationButton} onPress={handleShowLocation}>
-              <Ionicons name="locate-outline" size={24} color="black" />
-            </TouchableOpacity>
-          </View>
-          <TouchableOpacity style={styles.signInButton} onPress={handleContinuePress}>
-            <Text style={styles.signInButtonText}>CONTINUE</Text>
-          </TouchableOpacity>
-        </View>
-      );
     };
-    
-    const styles = StyleSheet.create({
-      container: {
+
+    const handleSearch = () => {
+        // Handle search logic here
+        console.log('Searching for:', searchQuery);
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.searchBar}>
+            {/* <Ionicons name="search" size={24} color="black" onPress={handleSearch} /> */}
+                <TextInput
+                    style={styles.input}
+                    placeholder="Search..."
+                    onChangeText={setSearchQuery}
+                    value={searchQuery}
+                />
+            </View>
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+                ref={mapRef}
+            >
+                <Marker
+                    coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+                    title="Marker"
+                    description="This is a marker"
+                />
+            </MapView>
+            <View style={styles.searchButton}>
+                
+            </View>
+            <TouchableOpacity style={styles.locationButton} onPress={handleShowLocation}>
+                <Ionicons name="locate-outline" size={24} color="black" />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.signInButton} onPress={handleContinuePress}>
+                <Text style={styles.signInButtonText}>CONTINUE</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
         flex: 1,
         backgroundColor: '#F0F0F0', // Background color
-      },
-      searchBar: {
+    },
+    searchBar: {
+        flexDirection: 'row',
         paddingHorizontal: 20,
-        paddingTop: 20,
+        paddingTop: 15,
         paddingBottom: 10,
-        backgroundColor: 'white', // Background color for search bar
-      },
-      mapContainer: {
+        backgroundColor: 'transparent', // Transparent background for search bar
+        position: 'absolute',
+        top: 39,
+        left: 0,
+        right: 0,
+        zIndex: 1, // Ensure search bar stays above map
+    },
+    input: {
         flex: 1,
-      },
-      map: {
+        height: 40,
+        borderRadius: 8,
+        paddingHorizontal: 10,
+        backgroundColor: '#F0F0F0',
+    },
+    map: {
         flex: 1,
-      },
-      locationButton: {
+    },
+    searchButton: {
+        position: 'absolute',
+        top: 58,
+        right: 26,
+        backgroundColor: '#F0F0F0',
+        zIndex: 1, // Ensure search button stays above map
+    },
+    locationButton: {
         position: 'absolute',
         bottom: 115, // Adjust this value to move the location button higher
         right: 20,
@@ -86,8 +116,8 @@ const OrganiseMap = (navigation) => {
         borderRadius: 50,
         padding: 10,
         elevation: 5,
-      },
-      signInButton: {
+    },
+    signInButton: {
         position: 'absolute',
         bottom: 20,
         backgroundColor: '#FF4459',
@@ -96,15 +126,14 @@ const OrganiseMap = (navigation) => {
         marginVertical: 10,
         width: '90%', // Set the width to 80% of the screen width
         alignSelf: 'center', // Center the button horizontally
-      },
-      signInButtonText: {
+    },
+    signInButtonText: {
         fontFamily: 'Poppins',
         fontSize: 22,
         fontWeight: '500',
         textAlign: 'center',
         color: 'white',
-      },
-    });
+    },
+});
 
-
-export default OrganiseMap
+export default OrganiseMap;
